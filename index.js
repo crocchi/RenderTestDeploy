8f097deae1ffdb2254f86c237efea52d6c324b53
crocchi <ziocro@gmail.com>
2023-11-25 01:53:43 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,18 +6,31 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+//  socket.io
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 
 // use res.render to load up an ejs view file
-//<%= token %>   <%  if (page == 'home') { %>
+
 // index page
 app.get('/', function(req, res) {
+    
+   // console.log(req.rawHeaders);
   res.render('pages/index',{
     msg:req.rawHeaders
   });
 });
-//console.log(req.rawHeaders);
 
-app.listen(8000);
-console.log('u Server sta ascoltando un pò di music sulla porta: 8000');
+
+server.listen(8000);
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+  });
+
+console.log('Server is listening on port 8000');
