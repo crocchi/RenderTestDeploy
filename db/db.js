f41f2d134a6626a5b585626a4c9ce1c1271914a4
crocchi <ziocro@gmail.com>
2023-11-26 22:11:18 +0100

    aggiornamenti

--- db/db.js
+++ db/db.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose')
 mongoose.set('strictQuery', true);
-const uri = process.env.MONGO_SRC;
+const uri = process.env.MONGO_SRC || "mongodb+srv://ziocro:9bMY0VBNDWe2z8vj@cluster0.hyj19vr.mongodb.net/?retryWrites=true&w=majority";
 
 mongoose
-    .connect(uri, { useNewUrlParser: true })
+    .connect(uri, /*{ useNewUrlParser: true }*/)
     .catch(e => {
         console.error('Connection error', e.message)
     })
