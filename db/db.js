fc52c0eed8bd845aff29a3808be5215539689a74
crocchi <ziocro@gmail.com>
2023-11-27 16:57:47 +0100

    new front template

--- db/db.js
+++ db/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 mongoose.set('strictQuery', true);
-const URI = process.env.MONGO_SRC ;
+const URI = process.env.MONGO_SRC || "mongodb+srv://ziocro:9bMY0VBNDWe2z8vj@cluster0.hyj19vr.mongodb.net/?retryWrites=true&w=majority";
 
 mongoose
     .connect(URI, /*{ useNewUrlParser: true }*/)
